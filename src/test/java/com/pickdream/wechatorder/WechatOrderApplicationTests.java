cc7f8be8cba314182a10b609291279b07be0ad50
maoxin <lumaoxin@aliyun.com>
2019-10-18 16:33:16 +0800

    Project init

---
+++ src/test/java/com/pickdream/wechatorder/WechatOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickdream.wechatorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
